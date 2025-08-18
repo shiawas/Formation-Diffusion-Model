d710ddb78f5aea3e42d76b28562c8456ce1dcfaa
Do Quang Hieu <shiawasaw@gmail.com>
2025-08-18 12:47:13 +0700

    update new visual

--- utils/visualization.py
+++ utils/visualization.py
@@ -22,21 +22,23 @@ def visualize_quadrotor_simulation_result(
     plt.gca().set_aspect("equal", adjustable="box")
 
 
+    # unpack state
     xs = [s[0] for s in states]  
     ys = [s[2] for s in states]
     phis = [s[4] for s in states]
-    # Generate circle for CBF
+
+    # obstacles
     plot_obstacles(obs_center, obs_radius)
 
-    # Plot the trajectory
-    plt.scatter(xs, ys, s=10, color="blue", alpha=0.6)
+    # compute trajectory at FRONT (not center)
+    front_xs = [x + quadrotor.l_q * np.cos(phi) for x, phi in zip(xs, phis)]
+    front_ys = [y + quadrotor.l_q * np.sin(phi) for y, phi in zip(ys, phis)]
 
-    # Plot quadrotor pose
-    for (x, y, phi) in zip(xs[::10], ys[::10], phis[::10]):
-        # front midpoint (this is the trajectory point)
-        front_x = x + quadrotor.l_q * np.cos(phi)
-        front_y = y + quadrotor.l_q * np.sin(phi)
+    # plot trajectory using FRONT
+    plt.scatter(front_xs, front_ys, s=10, color="blue", alpha=0.6)
 
+    # plot quadrotor poses
+    for (front_x, front_y, phi) in zip(front_xs[::10], front_ys[::10], phis[::10]):
         # half width vector (perpendicular to heading)
         dx = (quadrotor.w_q / 2) * -np.sin(phi)
         dy = (quadrotor.w_q / 2) *  np.cos(phi)
@@ -47,13 +49,12 @@ def visualize_quadrotor_simulation_result(
         p3 = (p2[0] - 2*quadrotor.l_q*np.cos(phi), p2[1] - 2*quadrotor.l_q*np.sin(phi))  # back-right
         p4 = (p1[0] - 2*quadrotor.l_q*np.cos(phi), p1[1] - 2*quadrotor.l_q*np.sin(phi))  # back-left
 
-        # draw rectangle
         rect_x = [p1[0], p2[0], p3[0], p4[0], p1[0]]
         rect_y = [p1[1], p2[1], p3[1], p4[1], p1[1]]
         plt.plot(rect_x, rect_y, color="red", alpha=0.6)
 
-    # plot start and end point
-    init_x, init_y = states[0][0], states[0][2]
+    # plot start and target using FRONT
+    init_x, init_y = front_xs[0], front_ys[0]
     plt.scatter(init_x, init_y, s=200, color="green", alpha=0.75, label="init. position")
     plt.scatter(5.0, 5.0, s=200, color="purple", alpha=0.75, label="target position")
 
