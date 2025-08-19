513cb36872bc36e1e31ac1b26a97d049628a5850
Do Quang Hieu <shiawasaw@gmail.com>
2025-08-19 18:44:24 +0700

    update new

--- formation/bearing_material/signum_term.py
+++ formation/bearing_material/signum_term.py
@@ -1,6 +1,6 @@
 import numpy as np
 from formation.bearing_material.lambda_min import compute_lambda_min
-from formation.bearing_material.make_g_list import make_g_list
+from formation.bearing_material.make_g_list import make_g_list_and_norm
 from formation.bearing_material.projection import Pr
 
 
@@ -19,7 +19,7 @@ def signum_term(H, p, g_star, beta_c):
     leaders = [0, 1]  # Python index (MATLAB 1,2)
     followers = [i for i in range(n) if i not in leaders]
 
-    g_list = make_g_list(H, p)
+    g_list, _ = make_g_list_and_norm(H, p)
 
     for i in followers:
         lambda_i = compute_lambda_min(i, H, g_list)
