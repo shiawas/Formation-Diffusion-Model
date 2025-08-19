3a03c330cae8d88c61354b4d591e092241cdb18e
Do Quang Hieu <shiawasaw@gmail.com>
2025-08-19 19:47:59 +0700

    update new

--- formation/bearing_material/make_g_dot_list.py
+++ formation/bearing_material/make_g_dot_list.py
@@ -5,22 +5,16 @@ def make_dot_g_list(H, v, g_list, norm_list):
     m, _ = H.shape
     d = v.shape[0]
     dot_g_list = []
-    idx = 0
     for k in range(m):
-        i_idx = np.where(H[k, :] == -1)[0]
-        j_idx = np.where(H[k, :] == 1)[0]
-        if len(i_idx) == 0 or len(j_idx) == 0:
-            continue
-        i = i_idx[0]
-        j = j_idx[0]
-        dot_z = v[:, j] - v[:, i]
-        g_k = g_list[idx]
-        P_g = Pr(g_k)
-        nrm = norm_list[idx]
+        i = np.where(H[k, :] == -1)[0][0]
+        j = np.where(H[k, :] == 1)[0][0]
+        dot_z = v[:, j] - v[:, i]  
+        g_k = g_list[k]            
+        P_g = Pr(g_k)             
+        nrm = norm_list[k]
         if nrm > 0:
-            dot_g = P_g @ dot_z / nrm
+            dot_g = P_g @ dot_z / nrm 
         else:
-            dot_g = np.zeros(d)
-        dot_g_list.append(dot_g)
-        idx += 1
+            dot_g = np.zeros(d)      
+        dot_g_list.append(dot_g)  
     return dot_g_list
