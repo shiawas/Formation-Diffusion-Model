6b0007e306f9c4a882c8e5f7ab8b2ef91f55dc03
Do Quang Hieu <shiawasaw@gmail.com>
2025-08-19 19:46:56 +0700

    update new

--- formation/bearing_material/make_g_list.py
+++ formation/bearing_material/make_g_list.py
@@ -10,7 +10,7 @@ def make_g_list_and_norm(H, p):
     for k in range(m):
         i = np.where(H[k, :] == -1)[0][0]
         j = np.where(H[k, :] ==  1)[0][0]
-        vec_ij = ( p[:, j] - p[:, i] ).reshape(d, 1, order = 'F')
+        vec_ij = ( p[:, j] - p[:, i] )
         nrm = np.linalg.norm(vec_ij)
         norm_list.append(nrm)
         if nrm > eps_norm:
