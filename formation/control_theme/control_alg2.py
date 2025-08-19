cde930e6fd391547eaa70cb56ff591e437fd8d27
Do Quang Hieu <shiawasaw@gmail.com>
2025-08-20 00:13:17 +0700

    new control law that maybe track the trajetory? idk just guess

--- formation/control_theme/control_alg2.py
+++ formation/control_theme/control_alg2.py
@@ -2,38 +2,62 @@ import numpy as np
 from formation.bearing_material.bearing_vector import br
 from formation.bearing_material.signum_term import signum_term
 from formation.bearing_material.projection import Pr
-from formation.bearing_material.lambda_min import compute_lambda_min
+from formation.bearing_material.g_utils import compute_g_and_gdot
+
 from scipy.linalg import block_diag
 
-def control_law_timevarying(p, v, H_bar, H, g_star, kp, kv, alpha):
-    d , _ = p.shape
+def control_law_timevarying(t, x, H, g_star, kp, kv, alpha):
+
+    d = 2 
+    n = 4
     m , _ = H.shape
 
-    g = np.zeros((d, m))
-    g_dot = np.zeros((d, m))
+    p = x[:d*n]
+    v = x[d*n : ]
 
-    for k in range(m):
-        i = np.where(H[k, :] == -1)[0][0]
-        j = np.where(H[k, :] ==  1)[0][0]
-        pij = (p[:, j] - p[:, i]).reshape(d,1,order='F')
-        vij = (v[:, j] - v[:, i]).reshape(d,1,order='F')
-        norm_pij = np.linalg.norm(pij)
-        if norm_pij > 1e-12:
-            gk = pij / norm_pij
-            g[:, k] = gk.flatten(order='F')
-            Pg = Pr(gk)
-            g_dot[:, k] = (Pg @ vij / norm_pij).flatten(order='F')
+    p = p.reshape(d, n,order='F')
+    v = v.reshape(d, n,order='F')
 
+    g, g_dot, norms = compute_g_and_gdot(H, p, v, eps=1e-9)
 
-    e1 = (g - g_star).reshape(d*m,1,order='F')
-    e2 = g_dot.reshape(d*m,1,order='F')
+    g_flat = np.concatenate(g)
+    dot_g_flat = np.concatenate(g_dot)
+    
+    g_star_flat = g_star.flatten(order = 'F')
+    sign_dot_g = np.sign(dot_g_flat)
+    H_bar = np.kron(H, np.eye(d))
 
-    term1 = -kp * (H_bar.T @ e1)
-    term2 = -kv * (H_bar.T @ e2)
-    term3 = -signum_term(H, p, g_star, alpha)
+    nl = 2
+    nf = n - nl
 
-    u = term1 + term2 + term3
-    return u
+    G = np.zeros((d*n, d*n))
+    G[d*nl:, d*nl:] = np.eye(d*nf)
+
+    term1 = -kp *   G @ H_bar.T @ (g_flat - g_star_flat)
+    term2 = -kv * G @ H_bar.T @ dot_g_flat
+    term3 = -alpha * G @ H_bar.T @ sign_dot_g
+
+    u = term1 + term2 + term3 # (8,)
+    
+    vr = np.array([
+        [0.35, 0.35], 
+        [0.3*np.cos(0.03*t), 0.3*np.cos(0.03*t)]
+    ])
+
+    vr_dot = np.array([
+        [0.0, 0.0],
+        [-0.009*np.sin(0.03*t), -0.009*np.sin(0.03*t)]
+    ])
+
+    dp = v
+    dv = u.reshape(d, n, order='F')
+    dp[:, :nl] = vr
+    dp[:, :nl] = vr_dot
+
+    dp = dp.flatten(order='F')
+    dv = dv.flatten(order='F')
+
+    return np.concatenate([dp,dv])
 
 
 
