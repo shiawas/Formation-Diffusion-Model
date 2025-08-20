abe629e596a3bb91e821c34b3fb4402806686efd
Do Quang Hieu <shiawasaw@gmail.com>
2025-08-20 13:42:54 +0700

    update control law

--- formation/control_theme/control_alg2.py
+++ formation/control_theme/control_alg2.py
@@ -5,30 +5,35 @@ from formation.bearing_material.projection import Pr
 from formation.bearing_material.g_utils import compute_g_and_gdot
 from scipy.interpolate import interp1d
 from formation.data_transformation.load_leader_trajectories import load_leader_trajectories
+from scipy.linalg import block_diag
 
 # using np.gradient to calculate a1_traj from p1dot from .csv file
 
-p1_func, p2_func, v1_func, v2_func, a1_func, a2_func = load_leader_trajectories("combined_data.csv", dt=0.01)
+p1_func, p2_func, v1_func, v2_func, phi_func, phi_dot_func = load_leader_trajectories("combined_data.csv", dt=0.01)
 
-def control_law_timevarying(t, x, H, g_star, kp, kv, alpha):
+def control_law_timevarying(t, x, H, g_star, kp, theta):
 
     d = 2 
     n = 4
     m , _ = H.shape
 
     p = x[:d*n]
-    v = x[d*n : ]
 
     p = p.reshape(d, n,order='F')
-    v = v.reshape(d, n,order='F')
 
-    g, g_dot, norms = compute_g_and_gdot(H, p, v, eps=1e-9)
-
-    g_flat = np.concatenate(g)
-    dot_g_flat = np.concatenate(g_dot)
+    p[:, 0] = p1_func(t)
+    p[:, 1] = p2_func(t)
     
+    v = np.zeros_like(p)
+
+    g, _ , _ = compute_g_and_gdot(H, p, v, eps=1e-9)
+
+    g_flat = g.flatten(order='F')
     g_star_flat = g_star.flatten(order = 'F')
-    sign_dot_g = np.sign(dot_g_flat)
+
+    P_blocks = [np.eye(d) - np.outer(g_star[:, k], g_star[:, k]) for k in range(m)]
+    P_diag_star = block_diag(*P_blocks)
+    
     H_bar = np.kron(H, np.eye(d))
 
     nl = 2
@@ -37,34 +42,24 @@ def control_law_timevarying(t, x, H, g_star, kp, kv, alpha):
     G = np.zeros((d*n, d*n))
     G[d*nl:, d*nl:] = np.eye(d*nf)
 
-    term1 = -kp *   G @ H_bar.T @ (g_flat - g_star_flat)
-    term2 = -kv * G @ H_bar.T @ dot_g_flat
-    term3 = -alpha * G @ H_bar.T @ sign_dot_g
+    q = P_diag_star @ g_flat
+    s = np.sign(q)
 
-    u = term1 + term2 + term3 # (8,)
+    term1 = -kp * G @ H_bar.T @ (g_flat - g_star_flat)
+    term2 = -theta * (G @ H_bar.T @ (P_diag_star @ s))
+
+    u = term1 + term2 # (8,)
     
-    # positions, velocities, accelerations of 2 leaders
-    p1 = p1_func(t)
-    p2 = p2_func(t)   
+    # velocities of 2 leaders  
     v1 = v1_func(t)
     v2 = v2_func(t)
-    a1 = a1_func(t)
-    a2 = a2_func(t)
 
-    pr = np.column_stack([p1, p2])
-    vr = np.column_stack([v1, v2])
-    vr_dot = np.column_stack([a1, a2])
 
-    dp = v
-    dv = u.reshape(d, n, order='F')
-    dp[:, :nl] = vr
-    dv[:, :nl] = vr_dot
-    p[:, :nl] = pr 
-    
-    dp = dp.flatten(order='F')
-    dv = dv.flatten(order='F')
+    dp = u.reshape(d, n, order='F')
+    dp[:, 0] = v1
+    dp[:, 1] = v2 
 
-    return np.concatenate([dp,dv])
+    return dp.flatten(order='F')
 
 
 
