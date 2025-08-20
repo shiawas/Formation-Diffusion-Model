8be4be9cd12496857c7ba358441f02d2b4413845
Do Quang Hieu <shiawasaw@gmail.com>
2025-08-20 12:32:31 +0700

    add phi

--- formation/data_transformation/load_leader_trajectories.py
+++ formation/data_transformation/load_leader_trajectories.py
@@ -13,9 +13,15 @@ def load_leader_trajectories(csv_file, dt=0.01):
     v1_traj = data[["p1_dot_x", "p1_dot_y"]].to_numpy()
     v2_traj = data[["p2_dot_x", "p2_dot_y"]].to_numpy()
     
+    phi_traj = data["phi"].to_numpy()
+    phi_dot_traj = data["phi_dot"].to_numpy()
+
     p1_func = interp1d(t_data, p1_traj, axis=0, fill_value="extrapolate")
     p2_func = interp1d(t_data, p2_traj, axis=0, fill_value="extrapolate")
     v1_func = interp1d(t_data, v1_traj, axis=0, fill_value="extrapolate")
     v2_func = interp1d(t_data, v2_traj, axis=0, fill_value="extrapolate")
+    phi_func = interp1d(t_data, phi_traj, axis=0, fill_value="extrapolate")
+    phi_dot_func = interp1d(t_data, phi_dot_traj, axis=0, fill_value="extrapolate")
 
-    return p1_func, p2_func, v1_func, v2_func
+
+    return p1_func, p2_func, v1_func, v2_func, phi_func, phi_dot_func
